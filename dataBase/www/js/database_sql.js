948d76a311179bbb734d9c3a0fff9befb18ef444
malithj <malithjayaweera@yahoo.com>
2015-02-21 19:28:30 +0530

    This is the first commit

---
+++ dataBase/www/js/database_sql.js
@@ -0,0 +1,212 @@
+var createStatement = "CREATE TABLE IF NOT EXISTS Contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, todoItem TEXT, reminder TEXT)";
+ 
+var selectAllStatement = "SELECT * FROM Contacts";
+ 
+var insertStatement = "INSERT INTO Contacts (todoItem, reminder) VALUES (?, ?)";
+ 
+var updateStatement = "UPDATE Contacts SET todoItem = ?, reminder = ? WHERE id=?";
+ 
+var deleteStatement = "DELETE FROM Contacts WHERE id=?";
+ 
+var dropStatement = "DROP TABLE Contacts";
+ 
+ var db = openDatabase("AddressBook", "1.0", "Address Book", 200000);  // Open SQLite Database
+ 
+var dataset;
+ 
+var DataType;
+ 
+ function initDatabase()  // Function Call When Page is ready.
+ 
+{
+ 
+    try {
+ 
+        if (!window.openDatabase)  // Check browser is supported SQLite or not.
+ 
+        {
+ 
+            alert('Databases are not supported in this browser.');
+ 
+        }
+ 
+        else {
+ 
+            createTable();  // If supported then call Function for create table in SQLite
+ 
+        }
+ 
+    }
+ 
+    catch (e) {
+ 
+        if (e == 2) {
+ 
+            // Version number mismatch. 
+ 
+            console.log("Invalid database version.");
+ 
+        } else {
+ 
+            console.log("Unknown error " + e + ".");
+ 
+        }
+ 
+        return;
+ 
+    }
+ 
+}
+ 
+function createTable()  // Function for Create Table in SQLite.
+ 
+{
+ 
+    db.transaction(function (tx) { tx.executeSql(createStatement, [], showRecords, onError); });
+ 
+}
+ 
+function insertRecord() // Get value from Input and insert record . Function Call when Save/Submit Button Click..
+ 
+{
+ 
+        var todoItemtemp = $('input:text[id=todoitem]').val();
+ 
+        var remindertemp = $('input:text[id=reminder]').val();
+        db.transaction(function (tx) { tx.executeSql(insertStatement, [todoItemtemp, remindertemp], loadAndReset, onError); });
+ 
+        //tx.executeSql(SQL Query Statement,[ Parameters ] , Sucess Result Handler Function, Error Result Handler Function );
+ 
+}
+ 
+function deleteRecord(id) // Get id of record . Function Call when Delete Button Click..
+ 
+{
+ 
+    var iddelete = id.toString();
+ 
+    db.transaction(function (tx) { tx.executeSql(deleteStatement, [id], showRecords, onError); alert("Delete Sucessfully"); });
+ 
+    resetForm();
+ 
+}
+ 
+function updateRecord() // Get id of record . Function Call when Delete Button Click..
+ 
+{
+ 
+    var todoItemupdate = $('input:text[id=todoItem]').val().toString();
+ 
+    var reminderupdate = $('input:text[id=reminder]').val().toString();
+ 
+    var useridupdate = $("#id").val();
+ 
+    db.transaction(function (tx) { tx.executeSql(updateStatement, [todoItemupdateupdate, reminderupdate, Number(useridupdate)], loadAndReset, onError); });
+ 
+}
+ 
+function dropTable() // Function Call when Drop Button Click.. Talbe will be dropped from database.
+ 
+{
+ 
+    db.transaction(function (tx) { tx.executeSql(dropStatement, [], showRecords, onError); });
+ 
+    resetForm();
+ 
+    initDatabase();
+ 
+}
+ 
+function loadRecord(i) // Function for display records which are retrived from database.
+ 
+{
+ 
+    var item = dataset.item(i);
+ 
+    $("#todoItem").val((item['todoItem']).toString());
+ 
+    $("#reminder").val((item['reminder']).toString());
+ 
+    $("#id").val((item['id']).toString());
+ 
+}
+ 
+function resetForm() // Function for reset form input values.
+ 
+{
+ 
+    $("#todoItem").val("");
+ 
+    $("#reminder").val("");
+ 
+    $("#id").val("");
+ 
+}
+ 
+function loadAndReset() //Function for Load and Reset...
+ 
+{
+ 
+    resetForm();
+ 
+    showRecords()
+ 
+}
+ 
+function onError(tx, error) // Function for Hendeling Error...
+ 
+{
+ 
+    alert(error.message);
+ 
+}
+ 
+function showRecords() // Function For Retrive data from Database Display records as list
+ 
+{
+ 
+    $("#results").html('')
+ 
+    db.transaction(function (tx) {
+ 
+        tx.executeSql(selectAllStatement, [], function (tx, result) {
+ 
+            dataset = result.rows;
+ 
+            for (var i = 0, item = null; i < dataset.length; i++) {
+ 
+                item = dataset.item(i);
+ 
+                var linkeditdelete = '<li>' + item['todoItem'] + ' , ' + item['reminder'] + '    ' + '<a href="#" onclick="loadRecord(' + i + ');">edit</a>' + '    ' +
+ 
+                                            '<a href="#" onclick="deleteRecord(' + item['id'] + ');">delete</a></li>';
+ 
+                $("#results").append(linkeditdelete);
+ 
+            }
+ 
+        });
+ 
+    });
+ 
+}
+ 
+$(document).ready(function () // Call function when page is ready for load..
+ 
+{
+;
+ 
+    $("body").fadeIn(2000); // Fede In Effect when Page Load..
+ 
+    initDatabase();
+ 
+    $("#submitButton").click(insertRecord);  // Register Event Listener when button click.
+ 
+    $("#btnUpdate").click(updateRecord);
+ 
+    $("#btnReset").click(resetForm);
+ 
+    $("#btnDrop").click(dropTable);
+ 
+});
+ 
---
+++ dataBase/www/js/index_user_scripts.js
@@ -0,0 +1,19 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  Submit */
+    $(document).on("click", ".uib_w_8", function(evt)
+    {
+       /* your code goes here */ 
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
